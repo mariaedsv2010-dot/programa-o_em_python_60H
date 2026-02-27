7c23192f673503617042dd9310a92393acf19207
mariaedsv2010-dot <mariaedsv2010@gmail.com>
2026-02-27 17:21:56 -0300

    Add files via upload

---
+++ atividades.py
@@ -0,0 +1,24 @@
+#atividade 1
+#numero = int(input('digite um numero inteiro:'))
+#quadrado = numero **2
+#print('o quadrado do numero é:', quadrado)
+
+#atividade 2
+#nome = 'alicia'
+#sobrenome = 'guedes'
+#print(nome, sobrenome)
+
+#atividade 3 
+n1 = int(input('digite um numero inteiro:'))
+n2 = int(input('digite um numero inteiro:'))
+print(n1,n2)
+
+#atividade 4
+texto = 'python'
+numero = 10
+print(texto, numero)
+
+#atividade 5
+variavel = 'ana gosta de comer:'
+nome = input('digite oque ana gosta de comer:')
+print(variavel, nome)
