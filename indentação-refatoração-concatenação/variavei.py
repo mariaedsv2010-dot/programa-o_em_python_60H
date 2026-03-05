cba4d2bb07cc9b6b03aa1b52504ef68c9650b3ec
mariaedsv2010-dot <mariaedsv2010@gmail.com>
2026-03-05 16:27:47 -0300

    Add files via upload

---
+++ indentação-refatoração-concatenação/variavei.py
@@ -0,0 +1,56 @@
+x = 4
+y = 6
+resultado = x * y / 2
+print('resultado:',x * y /2)
+
+numeros = [5,7]
+soma = sum(numeros)
+print('resultado:', soma)
+
+numeros = 3**4
+print('resultado:', numeros)   
+
+n1= 8
+n2 = 12
+n3 = 15 
+media= n1+n2+n3
+print('resultado:',media/3)
+
+n4 = 10
+n5 = 2
+resultado = n5-n4*5
+print('resultado:', resultado)
+
+
+n6 = 27
+n7 = 3
+resultado = n6/n7+5
+print('resultado:', resultado)
+
+n= 17%4
+print(n)
+
+n8= 9*3**2
+print(n8)
+
+n9 = 3*4+20
+print(n9)
+
+n10= 15*2-7**5
+print(n10)
+
+
+media = 17+21+25/3
+print(media)
+
+
+n11= 11*2+7
+print(n11)
+
+
+n12= 3*8-15/2
+print(n12)
+
+
+n13= 2**10
+print(n13)
