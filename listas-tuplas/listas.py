b89dd4b767c7827d1478fbbaa9c0efaf0b62a2d8
mariaedsv2010-dot <mariaedsv2010@gmail.com>
2026-03-06 14:33:24 -0300

    Add files via upload

---
+++ listas-tuplas/listas.py
@@ -0,0 +1,21 @@
+#ativiadade 0
+l = list(range(2,21,2))
+print(l)
+
+#atividade 2
+
+c = [1,2,3,4,5,6,7,8,9,10]
+print(c)
+
+primeiro_elemento = c [3]
+print(primeiro_elemento)
+
+c.append(9)
+print(c)
+
+c.remove(5)
+print(c)
+
+
+carros = ['ferrari,mercedes,jeep']
+print(carros,c)
